4a5a1b080acf0672fd78f34b6ec23703dd1bae79
r2coder <r2coder@163.com>
2025-01-19 10:14:20 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: u32) -> u32 {
+    let price = if count > 40 { 1 } else { 2 };
+    count * price
+}
 
 // Don't modify this function!
 #[test]
